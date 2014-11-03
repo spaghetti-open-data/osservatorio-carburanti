4408d2d60a66bd013a54fd1810051d2471d552c5
lorenzo perone <lorenzo.perone@gmail.com>
2014-11-03 20:32:11 +0100

    corretti i tipi

--- Analisi prezzi/analisi_carburanti_postgresql.sql
+++ Analisi prezzi/analisi_carburanti_postgresql.sql
@@ -1,18 +1,18 @@
 CREATE TABLE "public"."distributori_" (
-	"id" smallint,
-	"name" varchar(150),
-	"bnd" varchar(30),
-	"lat" double precision,
-	"lon" double precision,
-	"addr" varchar(150),
-	"comune" varchar(30),
-	"provincia" varchar(4));
+	"id" integer,
+	"name" text,
+	"bnd" text,
+	"lat" numeric,
+	"lon" numeric,
+	"addr" text,
+	"comune" text,
+	"provincia" text);
 
 
 CREATE TABLE "public"."prezzi_" (
   "id_d" bigint,
-  "dIns" date,
-  "carb" varchar(20),
-  "isSelf" smallint,
-  "prezzo" double precision,
-  "dScrape" smallint);
+  "dins" timestamp,
+  "carb" text,
+  "isself" integer,
+  "prezzo" numeric,
+  "dscrape" bigint);
